c178e98c8c79751f4e0c31bab75021120fdb6365
isurojit <iamsurojit@hotmail.com>
2020-08-25 21:11:35 +0530

    Section 2 chapter 11 String Methods and concatination

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+//Math Object-chapter 10
+/*
 const num1 = 100;
 const num2 = 50;
 
@@ -15,5 +17,64 @@ val = Math.max(2,4,5,6,7,25); // return Maximum number
 val = Math.random();// return random number
 
 val = Math.round(Math.random()*20 + 1); // return random number upto 20
+
+console.log(val); */
+
+
+//String and Concatination
+const firstName = "Surojit";
+const lastName = "Manna";
+const age = 36;
+const str = 'Hi my name is surojit'
+
+let val;
+
+val = firstName + lastName;
+
+//Concatination
+val = firstName+ " "+lastName;
+
+//Append
+val = 'Web Developer ';
+val += 'Surojit Manna';
+
+val = 'Hello, my name is ' + firstName + 'and  I am '+age;
+
+//Length
+val = firstName.length;
+
+//concat method
+val = firstName.concat(' ', lastName);
+
+//toupper
+val = firstName.toUpperCase();
+val = firstName.toLowerCase();
+
+val = firstName[0].toLowerCase();
+
+//indexof()
+
+val= firstName.indexOf('i');
+
+//charAt
+val =  firstName.charAt('2').toUpperCase();
+
+//Last character of string
+val = firstName.charAt(firstName.length -1).toUpperCase();
+
+//sub string
+val = firstName.substring(0, 3);
+
+//slice()
+val = firstName.slice(0, 4);
+
+//split()
+val = str.split(' ')
+
+//replace()
+val=  str.replace('Hi', 'bye');
+
+//Includes()
+val = str.includes('Hi');
 
 console.log(val);
