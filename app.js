04e58ce7b1cc727edf96a213f1ab1c23545f9bea
isurojit <iamsurojit@hotmail.com>
2020-08-25 20:53:45 +0530

    Section 2 chapter 10 Number and The Math Object

---
+++ app.js
@@ -0,0 +1,19 @@
+const num1 = 100;
+const num2 = 50;
+
+//Math object
+val = Math.PI;
+val = Math.E;
+val = Math.round(2.4);// Round up the number
+val = Math.ceil(2.4);// Round up to next incremented digit
+val = Math.floor(2.8);// Round up to previous demoted digit
+val = Math.sqrt(64);// Find the squre root
+val = Math.abs(-3);// Convert Negative number to positive.
+val = Math.pow(8, 2); // Give result of any powered number.
+val = Math.min(2,4,5,6,7,25); // return Minimum number
+val = Math.max(2,4,5,6,7,25); // return Maximum number
+val = Math.random();// return random number
+
+val = Math.round(Math.random()*20 + 1); // return random number upto 20
+
+console.log(val);
