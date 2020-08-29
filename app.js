71b2c7a4202710e602151db9f1478b7fe8fcfdf3
isurojit <iamsurojit@hotmail.com>
2020-08-29 11:47:10 +0530

    scope

--- app.js
+++ app.js
@@ -509,3 +509,34 @@ console.log(`Today Is ${day}`);
 // val = window.navigator.language;
 
 // console.log(val);
+
+//scope
+//Global Scope
+
+// var a = 1;
+// let b = 2;
+// const c = 3;
+
+// // function test(){
+// // 	var a = 4;
+// // 	let b = 5;
+// // 	const c = 6;
+// // 	console.log('Function Scope:',a,b,c);
+// // }
+
+// // test();
+
+// // if(true){
+// // 	//block scope
+// // 	var a = 4; // changed in global scope
+// // 	let b = 5;
+// // 	const c = 6;
+// // 	console.log('Block Scope:',a,b,c);
+// // }
+
+// for (let a = 0; a < 10;a++){
+// 	console.log(`Loop: ${a}`);
+// }
+
+
+// console.log('Global Scope:',a,b,c);
